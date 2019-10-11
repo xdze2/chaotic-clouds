5cec835bdd6081dd05303c9e2727ccfdc6b529e8
etienne <xdze2.me@gmail.com>
2019-10-11 23:33:59 +0200

    a simple butterfly

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 hello! :eyes:
 
 
-
 this is a [git submodule](https://git-scm.com/book/en/v2/Git-Tools-Submodules)
 and a [web page](https://xdze2.github.io/chaotic-clouds/)
+
+there is a standard [lion](./lion.html) and a [white butterfly](./white_lorenz.html)
---
+++ pointclouds/white_lorenz/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 359232,
+    "boundingBox": {
+        "lx": 67.0,
+        "ly": -12.0,
+        "lz": 13.0,
+        "ux": 649.0,
+        "uy": 570.0,
+        "uz": 595.0
+    },
+    "tightBoundingBox": {
+        "lx": 67.0,
+        "ly": -12.0,
+        "lz": 13.0,
+        "ux": 490.0,
+        "uy": 570.0,
+        "uz": 519.0
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 5.0402679443359379,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
