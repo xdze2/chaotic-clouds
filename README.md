6f26ad746941eba4e9a4b2709cd00a6662a2e5ad
etienne <xdze2.me@gmail.com>
2019-10-12 14:40:14 +0200

    color chemical

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ this is a [git submodule](https://git-scm.com/book/en/v2/Git-Tools-Submodules)
 and a [web page](https://xdze2.github.io/chaotic-clouds/)
 
 there is a standard [lion](./lion.html) and a [white butterfly](./white_lorenz.html)
+
+
+[Colored version](./chemical_lorenz_v02.html) using sort of chemical reaction
---
+++ pointclouds/colored_lorenz_v02/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 992032,
+    "boundingBox": {
+        "lx": 105.0,
+        "ly": -20.0,
+        "lz": 28.0,
+        "ux": 1045.0,
+        "uy": 920.0,
+        "uz": 968.0
+    },
+    "tightBoundingBox": {
+        "lx": 105.0,
+        "ly": -20.0,
+        "lz": 28.0,
+        "ux": 788.0,
+        "uy": 920.0,
+        "uz": 843.0
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 8.14063835144043,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
