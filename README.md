d40944fdb3d323c7a32798516fefe4dce8f96d17
etienne <xdze2.me@gmail.com>
2019-10-14 13:42:42 +0200

    add first version ThomasAttractor

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 hello! :eyes:
 
 
-this is a [git submodule](https://git-scm.com/book/en/v2/Git-Tools-Submodules)
+This is a [git submodule](https://git-scm.com/book/en/v2/Git-Tools-Submodules)
 and a [web page](https://xdze2.github.io/chaotic-clouds/)
 
-there is a standard [lion](./lion.html) and a [white butterfly](./white_lorenz.html)
+There is a standard [lion](./lion.html) and a [white butterfly](./white_lorenz.html)
 
 
 [Colored version](./chemical_lorenz_v02.html) using sort of chemical reaction
+
+
+
+[low-res and blue Thomas' attractor](./blue_thomas_attractor.html)
---
+++ pointclouds/thomas_blue1000/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 716941,
+    "boundingBox": {
+        "lx": -4.0656,
+        "ly": -4.0656,
+        "lz": -4.0656,
+        "ux": 4.0572,
+        "uy": 4.0572,
+        "uz": 4.0572
+    },
+    "tightBoundingBox": {
+        "lx": -4.0656,
+        "ly": -4.0656,
+        "lz": -4.0656,
+        "ux": 4.0572,
+        "uy": 4.0572,
+        "uz": 4.0572
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.07034551352262497,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
